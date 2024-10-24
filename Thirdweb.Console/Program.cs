821ba3be5a01e2f57288db86d3c50654cfcba458
0xFirekeeper <0xFirekeeper@gmail.com>
2024-10-24 18:00:41 +0700

    Twitch Login

--- Thirdweb.Console/Program.cs
+++ Thirdweb.Console/Program.cs
@@ -329,7 +329,7 @@ var privateKeyWallet = await PrivateKeyWallet.Generate(client: client);
 
 #region InAppWallet - OAuth
 
-// var inAppWalletOAuth = await InAppWallet.Create(client: client, authProvider: AuthProvider.Github);
+// var inAppWalletOAuth = await InAppWallet.Create(client: client, authProvider: AuthProvider.Twitch);
 // if (!await inAppWalletOAuth.IsConnected())
 // {
 //     _ = await inAppWalletOAuth.LoginWithOauth(
